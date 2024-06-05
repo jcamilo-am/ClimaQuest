3164eb4a657f30fc63082307d494cc3e4292f1e4
jcamilo-am <jcamarantom22@itp.edu.co>
2024-06-04 20:41:36 -0500

    fix

--- src/components/SearchSuggestion.tsx
+++ src/components/SearchSuggestion.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const SearchSuggestion = ({info, search }:{info: any[], search: string}) => {
     return <>
-      {info.length === 0 && <p className="text-gray-700 dark:text-slate-200 mt-2">No se encontraron resultados para '{search}'</p>}
+      {info.length === 0 && <p className="text-gray-700 dark:text-slate-200 mt-2">No se encontraron resultados para <i>{search}</i></p>}
       {info.map(({name, adm_area1, country, lat, lon, place_id}) => (
         <Link href={`/search/${place_id}`} key={place_id} className="w-full max-w-xl mx-auto mt-4 p-4 bg-slate-100 dark:bg-gray-900 border border-gray-300 dark:border-gray-700 rounded-md shadow-md">
           <div className="flex justify-between">
